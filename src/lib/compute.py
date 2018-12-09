cd30bd1f88769228595b65ce11f81f8c05740971
Pillman Huh <phuh56@gmail.com>
2018-12-10 03:18:10 +0900

    Add substraction support

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -6,20 +6,19 @@ class compute:
     def add(self):
         pass
 
-    def substract(self):
-         difference = 0
+    def subtract(self):
+        difference = 0
         for item in self.operands:
             difference -= item
         print(difference)
-
 
     def divide(self):
         pass
 
     def multiply(self):
-       if self.operands is None:
+        if self.operands is None:
             return
         product = 1
         for item in self.operands:
             product *= item
-        print(product)   
+        print(product)
